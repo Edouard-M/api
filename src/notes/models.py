8a19683cdcfe25a09bb1e7352ff43321c78ef560
Edouard Mignien <edou17@laposte.net>
2023-09-05 15:03:35 +0200

    add Sign-In Client

--- src/notes/models.py
+++ src/notes/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Client(models.Model):
-    name = models.CharField(null=False, max_length=20)
+    username = models.CharField(primary_key=True, null=False, max_length=20)
     email = models.CharField(null=True, max_length=50)
     password = models.CharField(null=False, max_length=50)
 
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"<Client: {self.name}, email={self.email} ({self.created})>"
+        return f"<Client: {self.username}, email={self.email} ({self.created})>"
--- src/notes/views.py
+++ src/notes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Client
 
 
 def notes(request):
@@ -23,6 +24,21 @@ def sign_in(request):
 
     context = {
         "title": "Sign In",
+        "error": None,
     }
+
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        email = request.POST.get("email", None)
+        password = request.POST.get("pwd", None)
+        print(f"<Username = {username} - email={email} - password={password}>")
+
+        if username and password:
+            try:
+                Client.objects.create(username=username, password=password)
+            except Exception as er:
+                print(f"Error: {er}")
+                context["error"] = er
+                # raise Exception(er)
 
     return render(request, "notes/sign_in.html", context=context)
