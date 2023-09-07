a800a875ae8efb97bc3f91dbea1c9c7a824e2933
Edouard Mignien <edou17@laposte.net>
2023-09-07 21:28:22 +0200

    add models foreingkeys

--- src/notes/admin.py
+++ src/notes/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, Tag, Note
 
 admin.site.register(Client)
+admin.site.register(Tag)
+admin.site.register(Note)
--- src/notes/models.py
+++ src/notes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Client(models.Model):
     username = models.CharField(primary_key=True, null=False, max_length=20)
-    email = models.CharField(null=True, max_length=50)
+    email = models.EmailField(null=True, max_length=50)
     password = models.CharField(null=False, max_length=50)
 
     created = models.DateTimeField(auto_now_add=True)
@@ -13,3 +13,23 @@ class Client(models.Model):
             return f"<Client: {self.username}, email={self.email} ({self.created})>"
         else:
             return f"<Client: {self.username} ({self.created})>"
+
+
+class Tag(models.Model):
+    title = models.CharField(null=False, max_length=20)
+
+    client = models.ForeignKey(Client, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"<Tag: {self.title}>"
+
+
+class Note(models.Model):
+    title = models.CharField(null=False, max_length=20)
+    code = models.CharField(null=False, max_length=500)
+    description = models.CharField(null=True, max_length=1000)
+
+    tags = models.ManyToManyField(Tag, null=False)
+
+    def __str__(self):
+        return f"<Note: {self.title}, code: {self.code}, description: {self.description}>"
