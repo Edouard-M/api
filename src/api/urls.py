16eed43845eef2ad8e4e18912be89308bbb36767
Edouard Mignien <edou17@laposte.net>
2023-11-05 01:44:16 +0100

    add tests

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import index
+from .views import index, test
 
 urlpatterns = [
     path('', index, name="index"),
     path('admin/', admin.site.urls),
+    path('test/', test, name="test"),
     path('notes/', include("notes.urls")),
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "api/index.html", context={"title": "API HOME"})
+
+def test(request):
+    context = {
+        "title": "TEST",
+    }
+
+    return render(request, "api/test.html", context=context)
