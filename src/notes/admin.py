9ded9be02e354ff5cc5d67abdde57e639442e183
Edouard Mignien <61579235+Edouard-M@users.noreply.github.com>
2023-11-05 20:42:53 +0100

    add User login logout sign-in (#1)

--- src/notes/admin.py
+++ src/notes/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Client, Tag, Note
+from .models import Tag, Note
 
-admin.site.register(Client)
 admin.site.register(Tag)
 admin.site.register(Note)
--- src/notes/urls.py
+++ src/notes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import notes, log_in, sign_in, dashboard
+from .views import notes, log_in, sign_in, dashboard, logout_user
 
 urlpatterns = [
     path('', notes, name="notes"),
     path('log-in/', log_in, name="log-in"),
     path('sign-in/', sign_in, name="sign-in"),
+    path('log-out/', logout_user, name="log-out"),
     path('dashboard/', dashboard, name="dashboard"),
 ]
