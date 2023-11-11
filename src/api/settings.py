ed036d9f3666e9133e3a20c55014f0cd1bf7b233
Edouard Mignien <edou17@laposte.net>
2023-11-11 15:46:54 +0100

    install live reload

--- src/api/settings.py
+++ src/api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload', # live reload
     'django.contrib.staticfiles',
     'notes',
 ]
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript', # live reload
 ]
 
 ROOT_URLCONF = 'api.urls'
